219e08b3c47c519a653e60516b0b049429f17516
Keks <keks@htmlacademy.ru>
2022-09-30 14:49:30 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,13 +1,29 @@
-function randomInteger(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+//ссылка на источник
+//(https://learn.javascript.ru/task/random-int-min-max)
+//функция возвращающая случайное целое число из переданного диапазона включительно
+function getRandomInteger(min, max) {
+  const rand = min + Math.random() * (max + 1 - min);
   if (min < 0) {
-    return NaN
+    return NaN;
   } else if (max <= min) {
-    return NaN
+    return NaN;
   }
   return Math.floor(rand);
 }
-randomInteger(1, 3);
+getRandomInteger(1, 3);
 
-//ссылка на источник ниже
-(https://learn.javascript.ru/task/random-int-min-max)
+//функция проверяет строку на максимальную длинну
+function checkMaxLength (str,maxLength) {
+  if (str.length <= maxLength) {
+    return true;
+  } return false;
+}
+checkMaxLength();
+
+//функция проверяет строку на минимальную и максимальную длинну
+function checkMinMaxLength (str,maxLength) {
+  if (str.length >= 20 && str.length <= maxLength) {
+    return true;
+  } return false;
+}
+checkMinMaxLength();
