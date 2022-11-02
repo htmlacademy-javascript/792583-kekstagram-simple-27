b8108151bdacb58aa8427da2d45c3070be63f315
Vladislav Ivanishin <skytex51rus@gmail.com>
2022-11-02 21:17:52 +1000

    change func and fix bug

--- js/scale-effect.js
+++ js/scale-effect.js
@@ -1,58 +1,80 @@
-const btnSmaller = document.querySelector('.scale__control--smaller');
-const btnBigger = document.querySelector('.scale__control--bigger');
-const scaleTransform = document.querySelector('.scale__control--value');
-const imgUploadPreview = document.querySelector('.img-upload__preview img');
-
 const SCALE_RANGE = 25;
 const MIN_SCALE = 25;
 const MAX_SCALE = 100;
-let scaleTransformValue = 100;
+const DEFAULT_SCALE = 100;
 
-scaleTransform.value = `${scaleTransformValue}%`;
-
-btnSmaller.addEventListener('click', () => {
-  if (scaleTransformValue > MIN_SCALE) {
-    scaleTransformValue -= SCALE_RANGE;
-    scaleTransform.value = `${scaleTransformValue}%`;
-    imgUploadPreview.style.transform = `scale(${scaleTransformValue / 100})`;
-  }
-});
-
-btnBigger.addEventListener('click', () => {
-  if (scaleTransformValue < MAX_SCALE) {
-    scaleTransformValue += SCALE_RANGE;
-    scaleTransform.value = `${scaleTransformValue}%`;
-    imgUploadPreview.style.transform = `scale(${scaleTransformValue / 100})`;
-  }
-});
-
+const btnSmaller = document.querySelector('.scale__control--smaller');
+const btnBigger = document.querySelector('.scale__control--bigger');
+const scaleInput = document.querySelector('.scale__control--value');
+const image = document.querySelector('.img-upload__preview img');
 const inputChrome = document.querySelector('#effect-chrome');
-inputChrome.addEventListener('click', () => {
-  imgUploadPreview.classList.remove(imgUploadPreview.classList.item(0));
-  imgUploadPreview.classList.add('effects__preview--chrome');
-});
 const inputNone = document.querySelector('#effect-none');
-inputNone.addEventListener('click', () => {
-  imgUploadPreview.classList.remove(imgUploadPreview.classList.item(0));
-  imgUploadPreview.classList.add('effects__preview--none');
-});
 const inputSepia = document.querySelector('#effect-sepia');
-inputSepia.addEventListener('click', () => {
-  imgUploadPreview.classList.remove(imgUploadPreview.classList.item(0));
-  imgUploadPreview.classList.add('effects__preview--sepia');
-});
 const inputMarvin = document.querySelector('#effect-marvin');
-inputMarvin.addEventListener('click', () => {
-  imgUploadPreview.classList.remove(imgUploadPreview.classList.item(0));
-  imgUploadPreview.classList.add('effects__preview--marvin');
-});
 const inputPhobos = document.querySelector('#effect-phobos');
-inputPhobos.addEventListener('click', () => {
-  imgUploadPreview.classList.remove(imgUploadPreview.classList.item(0));
-  imgUploadPreview.classList.add('effects__preview--phobos');
-});
 const inputHeat = document.querySelector('#effect-heat');
-inputHeat.addEventListener('click', () => {
-  imgUploadPreview.classList.remove(imgUploadPreview.classList.item(0));
-  imgUploadPreview.classList.add('effects__preview--heat');
+
+const scaleImage = (value = DEFAULT_SCALE) => {
+  image.style.transform = `scale(${value / DEFAULT_SCALE})`;
+  scaleInput.value = `${value}%`;
+};
+
+const onBtnSmallerClick = () => {
+  const currentValue = parseInt(scaleInput.value, 10);
+  let newValue = currentValue - SCALE_RANGE;
+  if (newValue < MIN_SCALE) {
+    newValue = MIN_SCALE;
+  }
+  scaleImage(newValue);
+};
+
+const onBtnBiggerClick = () => {
+  const currentValue = parseInt(scaleInput.value, 10);
+  let newValue = currentValue + SCALE_RANGE;
+  if (newValue > MAX_SCALE) {
+    newValue = MAX_SCALE;
+  }
+  scaleImage(newValue);
+};
+
+const resetScale = () => {
+  scaleImage();
+};
+const resetEffect = () => {
+  image.classList.remove(image.classList.item(0));
+};
+
+btnSmaller.addEventListener('click', onBtnSmallerClick);
+btnBigger.addEventListener('click', onBtnBiggerClick);
+
+inputChrome.addEventListener('click', () => {
+  resetEffect();
+  image.classList.add('effects__preview--chrome');
 });
+
+inputNone.addEventListener('click', () => {
+  resetEffect();
+  image.classList.add('effects__preview--none');
+});
+
+inputSepia.addEventListener('click', () => {
+  resetEffect();
+  image.classList.add('effects__preview--sepia');
+});
+
+inputMarvin.addEventListener('click', () => {
+  resetEffect();
+  image.classList.add('effects__preview--marvin');
+});
+
+inputPhobos.addEventListener('click', () => {
+  resetEffect();
+  image.classList.add('effects__preview--phobos');
+});
+
+inputHeat.addEventListener('click', () => {
+  resetEffect();
+  image.classList.add('effects__preview--heat');
+});
+
+export { resetScale, resetEffect };
